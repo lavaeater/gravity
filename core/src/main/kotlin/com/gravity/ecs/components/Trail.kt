5688e0d6031dd37d5ffdd6523a17e35ebffe3f69
Tommie Nygren <tommie.nygren@gmail.com>
2022-05-24 11:47:01 +0200

    Added trails and stuff

---
+++ core/src/main/kotlin/com/gravity/ecs/components/Trail.kt
@@ -0,0 +1,19 @@
+package com.gravity.ecs.components
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.utils.Pool
+import ktx.math.vec2
+
+class Trail: Component, Pool.Poolable {
+    var framesCounter = 0
+    val frameTarget = 5
+    var currentPointIndex = 0
+    val points = Array(200) {
+        vec2()
+    }
+    override fun reset() {
+        framesCounter = 0
+        currentPointIndex = 0
+        points.forEach { it.setZero() }
+    }
+}
---
+++ core/src/main/kotlin/com/gravity/ecs/systems/CameraFollowAnEntitySystem.kt
@@ -0,0 +1,48 @@
+package com.gravity.ecs.systems
+
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.core.EntitySystem
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.utils.viewport.ExtendViewport
+import com.gravity.ecs.components.Mass
+import com.gravity.ecs.components.Transform
+import com.gravity.injection.Context
+import ktx.ashley.allOf
+import ktx.ashley.mapperFor
+
+class CameraFollowAnEntitySystem: EntitySystem() {
+    val allFamily = allOf(Transform::class, Mass::class).get()
+    val massMapper = mapperFor<Mass>()
+    val allEntities get() = engine.getEntitiesFor(allFamily).sortedByDescending { massMapper.get(it).mass }
+    val transMapper = mapperFor<Transform>()
+    lateinit var selectedEntity : Entity
+    private val camera by lazy { Context.inject<OrthographicCamera>() }
+    private val viewPort by lazy { Context.inject<ExtendViewport>() }
+
+    var selectedEntityIndex = 0
+        set(value) {
+            var actualValue = value
+            if(actualValue > allEntities.size - 1)
+                actualValue = 0
+            if(actualValue < 0)
+                actualValue = allEntities.size - 1
+
+            selectedEntity = allEntities[actualValue]
+            field = actualValue
+        }
+
+    override fun update(deltaTime: Float) {
+        if(!::selectedEntity.isInitialized && allEntities.any()) {
+            selectedEntity = allEntities.first()
+        }
+
+        if(::selectedEntity.isInitialized) {
+            val pos = transMapper.get(selectedEntity).position
+            camera.position.set(pos.x / 10f, pos.y / 10f, 0f)
+
+            viewPort.update(Gdx.graphics.width, Gdx.graphics.height)
+            camera.update(true)
+        }
+    }
+}
