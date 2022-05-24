cc37201be704e3fcdcd2f08bfa99a5fdff044463
Tommie Nygren <tommie.nygren@gmail.com>
2022-05-24 11:52:34 +0200

    Wut wuth

--- core/src/main/kotlin/com/gravity/ecs/systems/GravitySystem.kt
+++ core/src/main/kotlin/com/gravity/ecs/systems/GravitySystem.kt
@@ -5,30 +5,35 @@ import com.badlogic.ashley.systems.IteratingSystem
 import com.gravity.ecs.components.Acceleration
 import com.gravity.ecs.components.Mass
 import com.gravity.ecs.components.Transform
+import com.gravity.injection.GameConstants.G
 import ktx.ashley.allOf
 import ktx.ashley.mapperFor
-
+import ktx.math.div
+import ktx.math.times
+import ktx.math.vec2
 
 class GravitySystem : IteratingSystem(allOf(Mass::class, Transform::class, Acceleration::class).get()) {
     private val entitiesWithMassFamily = allOf(Mass::class, Transform::class, Acceleration::class).get()
     private val transMapper = mapperFor<Transform>()
     private val massMapper = mapperFor<Mass>()
     private val accMapper = mapperFor<Acceleration>()
+    private val forceVector = vec2()
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
-        val mas1 = massMapper.get(entity)
-        val acc1 = accMapper.get(entity)
-        val tra1 = transMapper.get(entity)
+        forceVector.setZero()
+        val myMass = massMapper.get(entity)
+        val myAcceleration = accMapper.get(entity)
+        val myTransform = transMapper.get(entity)
 
         val allButMe = engine.getEntitiesFor(entitiesWithMassFamily) - entity
-        for(other in allButMe) {
-            val mas2 = massMapper.get(other)
-            val tra2 = transMapper.get(other)
-            val distance = tra1.position.dst(tra2.position)
-            val unitVector = tra2.position.cpy().sub(tra1.position).nor()
-            val newAcc = unitVector.scl (((mas1.mass * mas2.mass) / distance * distance))
-            acc1.a.add(newAcc)
+        for (other in allButMe) {
+            val otherMass = massMapper.get(other)
+            val otherTransform = transMapper.get(other)
+            val distance = myTransform.position.dst(otherTransform.position)
+            val unitVector = myTransform.position.cpy().sub(otherTransform.position).nor()
+            val thisForce = unitVector * (-G * ((myMass.mass * otherMass.mass) / (distance * distance)))
+            forceVector.add(thisForce)
         }
-
+        myAcceleration.a.set(forceVector / myMass.mass)
     }
 }
