af57aea2b2fffbeee135fcf54305f1111792896b
Tommie Nygren <tommie.nygren@gmail.com>
2022-05-24 08:19:05 +0200

    Fiddling

--- core/src/main/kotlin/com/gravity/injection/GameConstants.kt
+++ core/src/main/kotlin/com/gravity/injection/GameConstants.kt
@@ -1,7 +1,7 @@
 package com.gravity.injection
 
 object GameConstants {
-    const val MAX_DISTANCE = 100000f
+    const val MAX_DISTANCE = 10000000f
     const val GAME_HEIGHT = 100f
     const val GAME_WIDTH = 100f
     const val MIN_MASS = 1000f
